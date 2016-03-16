fdb6c47d5e934d1862d6df79d89704fdde10e2c7
Sandeep Kumar Vemula <bittu4u.4ever@gmail.com>
2016-03-16 23:59:32 +0530

    Socket setup done

--- components/App.js
+++ components/App.js
@@ -1,11 +1,40 @@
 var React = require('react');
+var io = require('socket.io-client');
+var Header = require('./parts/Header');
 
 var App = React.createClass({
-   render() {
+    
+    getInitialState() {
+        return {
+            status: 'disconnected'
+        }    
+    },
+    
+    componentWillMount() {
+        this.socket = io('http://localhost:3000');
+        this.socket.on('connect', this.connect);
+        this.socket.on('disconnect', this.disconnect);
+    },
+    
+    connect() {
+        this.setState({
+           status: 'connected' 
+        });
+    },
+    
+    disconnect() {
+        this.setState({
+           status: 'disconnected' 
+        });
+    },
+    
+    render() {
        return (
-           <h1> React app </h1>
+           <div>
+                <Header title="New Header" status={this.state.status}></Header>
+           </div>
        )
-   } 
+    } 
 });
 
 module.exports = App;
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
-
 var app = express();
 
-app.use(express.static('./public'));
+var connections = [];
 
-app.listen(3000);
+app.use(express.static('./public'));
+app.use(express.static('./node_modules/bootstrap/dist'));
+
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+    
+    socket.once('disconnect', function() {
+        connections.splice(connections.indexOf(socket), 1);
+        socket.disconnect();
+        console.log('disconnected %s sockets remaining', connections.length);
+    });
+    connections.push(socket);
+    console.log('connected %s sockets connected', connections.length);
+});
 console.log('server running... :3000');
